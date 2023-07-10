00b33ba5542a6d2641df274bf187fb41c5b6be90
Matthews Brito <109427217+Matthewsfx05@users.noreply.github.com>
2023-07-10 12:34:54 -0300

    Add files via upload

---
+++ Prova - Flavio Matthews/index.js
@@ -0,0 +1,33 @@
+// atvdd 1
+
+// atvdd 2
+
+// const url = require('url)
+// const address = "https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf" 
+
+// const objeto = {
+//     root: console.log('root:',''),
+//     dir: console.log('dir:',''),
+//     base: console.log('base:',''),
+//     ext: console.log('ext:',''),
+//     name: console.log('name:',''),
+// }
+
+// atvdd 3
+
+// atvdd 4 
+
+// const fs = require('fs')
+
+// const pes = ' melhor'
+// const fifa = 'pior'
+
+// fs.rename('prova.txt', 'arqv.txt', (err) =>{
+//     if (err) throw err;
+//         console.log('Arquivo renomeado!')
+// })
+
+//  atvdd 5
+
+// const os = require('os')
+// console.log(os.cpus())
